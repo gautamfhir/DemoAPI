90ac248b0aa1d2d107a1cd98154992e75c6e4f6a
Gautam <gautamfhir@outlook.com>
2020-07-21 09:47:05 -0700

    Updated

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DemoAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "stage", "prod" };
+            return new string[] { "stage1", "prod1" };
         }
 
         // GET api/values/5
